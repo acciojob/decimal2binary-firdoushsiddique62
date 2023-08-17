32a6e7942d644dfd483bfbcd30b8b5e9f94cb8c7
FIRDOUSH ALI SIDDIQUE <115089495+firdoushsiddique62@users.noreply.github.com>
2023-08-17 21:45:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function decimalToBinary(num) {
+ function decimalToBinary(num) {
   //Write you code here
-  
+	let ans = "";
+	while(num>=1){
+		ans = num%2 + ans;
+		num = Math.floor(num/2);
+// 		console.log(num);
+	}
+  return ans;
 }
 
 window.decimalToBinary = decimalToBinary;
